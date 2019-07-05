da97a53c4294f41062948d509e7f12c2146cb612
Hyunsung Kim <kokily@naver.com>
2019-07-05 18:00:39 +0900

    전표 입력

--- api/home.js
+++ api/home.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import Product from '../models/Product';
 
 const api = express.Router();
 
@@ -7,6 +8,75 @@ api.get('/', (req, res) => {
   res.render('home/index', {
     title: '행사전표 시스템에 오신 것을 환영합니다.'
   });
+});
+
+// 현역 메뉴 (GET) '/soldier'
+api.get('/soldier', (req, res) => {
+  Product.find({'native': '현역'}).distinct('division').exec()
+    .then((products) => {
+      res.render('home/soldier', {
+        title: '현역 전표 메뉴',
+        products
+      });
+    }).catch(err => console.error(err.stack));
+});
+
+// 현역 세부 메뉴 (GET) '/soldier/menu/:id'
+api.get('/soldier/menu/:id', (req, res) => {
+  Product.find({'native': '현역', 'division': req.params.id})
+    .sort({"id": 1}).exec()
+    .then((products) => {
+      res.render('home/menu', {
+        title: '현역 세부 메뉴표',
+        products
+      });
+    }).catch(err => console.error(err.stack));
+});
+
+// 예비역 메뉴 (GET) '/reserve'
+api.get('/reserve', (req, res) => {
+  Product.find({'native': '예비역'}).distinct('division').exec()
+    .then((products) => {
+      res.render('home/reserve', {
+        title: '예비역 전표 메뉴',
+        products
+      });
+    }).catch(err => console.error(err.stack));
+});
+
+// 예비역 세부 메뉴 (GET) '/reserve/menu/:id'
+api.get('/reserve/menu/:id', (req, res) => {
+  Product.find({'native': '예비역', 'division': req.params.id})
+    .sort({"id": 1}).exec()
+    .then((products) => {
+      res.render('home/menu', {
+        title: '예비역 세부 메뉴표',
+        products
+      });
+    }).catch(err => console.error(err.stack));
+});
+
+// 일반 메뉴 (GET) '/general'
+api.get('/general', (req, res) => {
+  Product.find({'native': '일반'}).distinct('division').exec()
+    .then((products) => {
+      res.render('home/general', {
+        title: '일반 전표 메뉴',
+        products
+      });
+    }).catch(err => console.error(err.stack));
+});
+
+// 일반 세부 메뉴 (GET) '/general/menu/:id'
+api.get('/general/menu/:id', (req, res) => {
+  Product.find({'native': '일반', 'division': req.params.id})
+    .sort({"id": 1}).exec()
+    .then((products) => {
+      res.render('home/menu', {
+        title: '일반 세부 메뉴표',
+        products
+      });
+    }).catch(err => console.error(err.stack));
 });
 
 export default api;
